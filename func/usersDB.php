c449abc1bea70253b78fe5128a5316a8971579fd
lVlist <mist45@mail.ru>
2018-12-25 22:33:31 +0300

    upd

--- func/usersDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    ['login' => 'admin', 'password' => 'budnik'],
-    ['login' => 'moderator', 'password' => 'DJSD&SD,.d,sdmsdfghsdfsdf'],
-    ['login' => 'user', 'password' => 'DJSD&SD,.d,sdmsdfghsdfsdf'],
-];
