2f6f4577a77394a6d00c22d32dbef0d670612372
lVlist <mist45@mail.ru>
2019-01-03 02:22:37 +0300

    upd

--- func/edit_game.php
+++ func/edit_game.php
@@ -2,8 +2,7 @@
 require('../conf/dbconfig.php');
 
 /* Записываем  название турнира */ 
-if ($_POST['new_game'])
-{
+if ($_POST['new_game']){
     $game = $conn->prepare("INSERT INTO games (game, date) VALUES (?,?)");
     $game->bind_param('ss',htmlspecialchars($_POST['new_game']), date("Y-m-d"));
     $game->execute();
@@ -12,8 +11,7 @@ if ($_POST['new_game'])
 }
 
 /* Обновление названия турнира */
-if($_POST['upd_game'])
-{
+if($_POST['upd_game']){
     $id_game = $_POST['upd_game'];
     $game = $_POST['game'];
     $conn->query("UPDATE `games` SET `game`= '$game' WHERE (`id`= $id_game)");
@@ -21,8 +19,7 @@ if($_POST['upd_game'])
 }
 
 /* Создание команды и запись в турнир */
-if($_POST['new_team'])
-{
+if($_POST['new_team']){
     //Создаем команду
     $team = $_POST['new_team'];
     $conn->query("INSERT INTO teams (team) VALUES ('$team')");
@@ -34,17 +31,14 @@ if($_POST['new_team'])
 }
 
 /* Добавление команды в турнир */
-if($_POST['add_team'])
-{
+if($_POST['add_team']){
     $id_game = $_POST['id_game'];
     $id_team = $_POST['add_team'];
     $team = $conn->query("SELECT qualification.id_team FROM qualification WHERE qualification.id_game = $id_game");
     $error = "";
-    foreach ($team as $value)
-    {
+    foreach ($team as $value){
         //echo $value['id_team']."-".$id_team."<br>";
-        if($value['id_team'] == $id_team)
-        {
+        if($value['id_team'] == $id_team){
             $error = true;
             break;
         }
@@ -60,17 +54,15 @@ if($_POST['add_team'])
 }
 
 /* Удаление команды */
-if($_POST['del_team'])
-{
+if($_POST['del_team']){
     $id_game = $_POST['id_game'];
     $id_team = $_POST['del_team'];
     $conn->query("DELETE FROM `qualification` WHERE (`id_game` = $id_game) AND (`id_team` = $id_team)");
     header('Location: ../admin/create.php?id='.$id_game);
 }
 
-
-if($_POST['del_game'])
-{
+/* Удаление игры */
+if($_POST['del_game']){
     $id_game = $_POST['del_game'];
     $conn->query("DELETE FROM `qualification` WHERE (`id_game` = $id_game)");
     $conn->query("DELETE FROM `q_games` WHERE (`id_game` = $id_game)");
