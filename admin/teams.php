dbe1f502af073ebd27191efda25727b9646ceb43
lVlist <mist45@mail.ru>
2019-04-11 13:00:38 +0300

    upd

--- admin/teams.php
+++ admin/teams.php
@@ -10,6 +10,7 @@ $login = getUserLogin();
 echo "<div id='main'>";
 if ($login != null){
     echo "<div id='create-block'>";
+    echo "Название команды изменяется сразу в появившемся поле с командой, только потом жмем \"Изменить\"<br>";
     echo "Команда:
     <input type='text' name='team' placeholder='Название команды' class='team input-team' autocomplete='off'>
     <div class='search_teams'></div>";
